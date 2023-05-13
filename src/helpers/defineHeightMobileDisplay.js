7628778976e215bf21d2bc18919d5edae971330a
Alexandr98k <oleksandr.karpenko98@gmail.com>
2023-05-13 22:40:08 +0300

    another fix bug

--- src/helpers/defineHeightMobileDisplay.js
+++ src/helpers/defineHeightMobileDisplay.js
@@ -3,8 +3,7 @@ const defineHeightMobileDisplay = function () {
   if (!isMobile) return;
 
   const setHeight = function () {
-    const currentHeight = window.innerHeight;
-    document.body.style.height = `${currentHeight}px`;
+    document.querySelector('.app ').style.height = `${window.innerHeight}px`;
   };
   window.addEventListener('resize', setHeight);
   //on start we also check the height of body
@@ -12,3 +11,23 @@ const defineHeightMobileDisplay = function () {
 };
 
 export default defineHeightMobileDisplay;
+
+// // define a function that sets min-height of my-element to window.innerHeight:
+
+// const setHeight = () => {
+//   document.getElementById('my-element').style.minHeight = window.innerHeight + 'px';
+// };
+
+// // define mobile screen size:
+
+// let deviceWidth = window.matchMedia('(max-width: 1024px)');
+
+// if (deviceWidth.matches) {
+//   // set an event listener that detects when innerHeight changes:
+
+//   window.addEventListener('resize', setHeight);
+
+//   // call the function once to set initial height:
+
+//   setHeight();
+// }
