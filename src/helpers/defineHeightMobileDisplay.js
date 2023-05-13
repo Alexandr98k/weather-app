51ff4651bc3599bf0a9e1f674fff804353bb43a5
Alexandr98k <oleksandr.karpenko98@gmail.com>
2023-05-13 22:51:00 +0300

    trying to fix bug

--- src/helpers/defineHeightMobileDisplay.js
+++ src/helpers/defineHeightMobileDisplay.js
@@ -1,10 +1,10 @@
 const defineHeightMobileDisplay = function () {
   const setHeight = function () {
-    document.querySelector('.app ').style.height = `${window.innerHeight}px`;
+    document.querySelector('.app ').style.minHeight = `${window.innerHeight}px`;
   };
 
   let deviceWidth = window.matchMedia('(max-width: 1024px)');
-
+  console.log(deviceWidth);
   if (deviceWidth.matches) {
     // set an event listener that detects when innerHeight changes:
     window.addEventListener('resize', setHeight);
