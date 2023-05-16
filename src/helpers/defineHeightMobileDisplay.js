c9944cad05783e7f71c472ff237ce6df510810cf
Alexandr98k <oleksandr.karpenko98@gmail.com>
2023-05-16 23:36:46 +0300

    try another way of fixing bug with 100vh height on mobile

--- src/helpers/defineHeightMobileDisplay.js
+++ src/helpers/defineHeightMobileDisplay.js
@@ -1,17 +1,40 @@
 const defineHeightMobileDisplay = function () {
+  const isMobile = navigator.userAgentData.mobile;
+  console.log(isMobile);
+  if (!isMobile) return;
+
   const setHeight = function () {
-    document.querySelector('.app ').style.minHeight = `${window.innerHeight}px`;
+    const currentHeight = window.innerHeight;
+    console.log(currentHeight);
+    document.querySelector('.app').style.minHeight = `${currentHeight}px`;
   };
+  window.addEventListener('resize', setHeight);
+  // const setHeight = function () {
+  //   document.querySelector('.app ').style.minHeight = `${window.innerHeight}px`;
+  // };
 
-  let deviceWidth = window.matchMedia('(max-width: 1024px)');
-  console.log(deviceWidth);
-  if (deviceWidth.matches) {
-    // set an event listener that detects when innerHeight changes:
-    window.addEventListener('resize', setHeight);
+  // let deviceWidth = window.matchMedia('(max-width: 1024px)');
+  // console.log(deviceWidth);
+  // if (deviceWidth.matches) {
+  //   // set an event listener that detects when innerHeight changes:
+  //   window.addEventListener('resize', setHeight);
 
-    // call the function once to set initial height:
-    setHeight();
-  }
+  //   // call the function once to set initial height:
+  //   setHeight();
+  // }
+  setHeight();
 };
 
 export default defineHeightMobileDisplay;
+
+// const defineHeightMobileDisplay = function () {
+//   const isMobile = navigator.userAgentData.mobile;
+//   console.log(isMobile);
+//   if (!isMobile) return;
+
+//   const setHeight = function () {
+//     const currentHeight = window.innerHeight;
+//     console.log(currentHeight);
+//     document.body.style.height = `${currentHeight}px`;
+//   };
+//   window.addEventListener('resize', setHeight);
